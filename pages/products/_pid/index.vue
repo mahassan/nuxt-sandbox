86c7330f69cf0057ed462e6a207953dae4feba2e
Ammar Hassan <ammar@ammarhassan.co.uk>
2018-11-08 21:29:44 +0000

    validate to check if entering params are valid

--- pages/products/_pid/index.vue
+++ pages/products/_pid/index.vue
@@ -3,3 +3,10 @@
         <h1>product ID is {{$route.params.pid}}</h1>
     </div>
 </template>
+<script>
+export default {
+    validate(data){
+        return data.params.pid == 1
+    }
+}
+</script>
