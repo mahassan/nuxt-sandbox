370113f1ce0570691bac4c9ebc200b725ed8ca6b
Ammar Hassan <ammar@ammarhassan.co.uk>
2018-11-08 21:16:56 +0000

    dynamic loading through url example

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,6 +1,20 @@
 <template>
     <div>
-        <nuxt-link to="/users">Load Product</nuxt-link>
-        <nuxt-link to="/users">Load Product 2</nuxt-link>
+        <nuxt-link to="/products/1">Load Product</nuxt-link>
+        <nuxt-link to="/products/2">Load Product 2</nuxt-link>
     </div>
-</template
+</template>
+
+<script>
+export default{
+      data(){
+        return {
+        }
+    },
+    methods: {
+        loadProduct(){
+            this.$router.param('/products/1');
+        }
+    }
+}
+</script>
