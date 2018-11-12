2ed192dea359db7229b34114bd7a4fc41bfe0d3c
Ammar Hassan <ammar@click.tech>
2018-11-12 15:15:18 +0000

    componentalized

--- pages/index.vue
+++ pages/index.vue
@@ -1,60 +1,40 @@
 <template>
-  <section class="container">
-    <div>
-      <input type="text" v-model="userId">
-      <button @click="onLoadUser">Load user</button> {{userId}}
-    </div>
-    <button @click="onLoadProducts">Products</button>
-  </section>
-
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech news</h1>
+    </section>
+    <section class="featured-posts"> 
+      <PostPreview
+      id="1"
+      thumbnail=""
+      title="Hi"
+      previewText="Demo"
+       />
+      <PostPreview
+      id="2"
+      thumbnail=""
+      title="Hello"
+      previewText="Demo"
+       />
+      <PostPreview
+      id="3"
+      thumbnail=""
+      title="sup"
+      previewText="Demo"
+       />
+    </section>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import PostPreview from '@/components/Posts/PostPreview'
 
 export default {
-  data(){
-    return {
-      userId : ''
-    }
-  },
-  methods: {
-    onLoadProducts(){
-      this.$router.push('/products/')
-    }
+  components: {
+    PostPreview
   }
 }
 </script>
 
 <style>
-
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
